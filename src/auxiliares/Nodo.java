acfae4c80d354ad68ca3b5fde173c16e25b17c89
Fede Parodi <fedejp_98@hotmail.com.ar>
2018-10-28 19:32:08 -0300

    Clases nodo y aristas
    Estas son las clases que voy a usar para la solución de UCS

---
+++ src/auxiliares/Nodo.java
@@ -0,0 +1,37 @@
+package auxiliares;
+
+import graficos.Punto;
+
+public class Nodo {
+	private Punto id; //El punto en sí mismo del nodo
+	private Punto predecesor; //El punto que lo precede en el camino
+	private int costo_acumulado; //El costo que tiene llegar desde el inicial hasta el nodo
+	
+	public Nodo(Punto id) {
+		this.id= id;
+		this.predecesor=null;
+		int costo_acumulado=Integer.MAX_VALUE; //Inicio el costo de llegada en infinito al crear el nodo
+	}
+
+	public Punto getPredecesor() {
+		return predecesor;
+	}
+
+	public void setPredecesor(Punto predecesor) {
+		this.predecesor = predecesor;
+	}
+	
+	public int getCosto_acumulado() {
+		return costo_acumulado;
+	}
+
+	public void setCosto_acumulado(int costo_acumulado) {
+		this.costo_acumulado = costo_acumulado;
+	}
+
+	public Punto getID() {
+		return id;
+	}
+
+
+}
