85690ca4475445a60b4e6cdac17d889025092e3f
Fede Parodi <fedejp_98@hotmail.com.ar>
2018-11-06 19:58:00 -0300

    Matrices retocadas
    Necesitamos encontrar la forma de modificar los nodos que recorrió sin que se rompa la lista y así obtener siempre el mejor camino.

--- src/solucion/NodoPunto.java
+++ src/solucion/NodoPunto.java
@@ -1,30 +1,43 @@
 package solucion;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import graficos.Punto;
 
-public class NodoPunto {
-	private List<Punto> puntos;
+
+public class NodoPunto implements Comparable<NodoPunto>{
+	//private List<Punto> puntos;
 	private NodoPunto predecesor;
 	private Punto p;
 	private int densidad;
+	private int distancia;
 	
-	public NodoPunto(Punto x, int dens, List<Punto> lista, NodoPunto prede){
+	public NodoPunto(Punto x, int dens) {
+		this.p = x;
+		this.densidad = dens;
+		this.predecesor = null;
+		//this.puntos = new ArrayList<Punto>();
+		//this.puntos.add(x);
+	}
+	
+	//public NodoPunto(Punto x, int dens, List<Punto> lista, NodoPunto prede){
+	public NodoPunto(Punto x, int dens, NodoPunto prede){
 		this.p = x;
 		this.densidad = dens;
 		this.predecesor = prede;
-		this.puntos = lista;
-		this.puntos.add(x);
+	//	this.puntos = new ArrayList<Punto>();
+	//	this.puntos.addAll(lista);
+	//	this.puntos.add(x);
 	}
 	
 	public NodoPunto getPredecesor() {
 		return predecesor;
 	}
 
-	public List<Punto> getPuntos() {
+	/*public List<Punto> getPuntos() {
 		return puntos;
-	}
+	}*/
 	
 	public Punto getP() {
 		return p;
@@ -33,4 +46,44 @@ public class NodoPunto {
 	public int getDensidad() {
 		return densidad;
 	}
+	
+	public void setDistancia(Punto destino) {
+		this.distancia = Math.abs(destino.x-p.x) + Math.abs(destino.y - p.y);
+	}
+	
+	public int relacionDistanciaDensidad() {
+			return distancia-densidad;
+	}
+	
+	@Override
+	public String toString() {
+		return "["+this.getP().x + ":" + this.getP().y + "]";
+	}
+	
+	@Override
+	public int compareTo(NodoPunto n){
+		if(n.relacionDistanciaDensidad() < this.relacionDistanciaDensidad()){
+			return 1;
+		}else if(n.relacionDistanciaDensidad() == this.relacionDistanciaDensidad()){
+			return 0;
+		}else{
+			return -1;
+		}
+	}
+
+	public void setPredecesor(NodoPunto nodo) {
+		this.predecesor=nodo;
+	}
+
+	public void setDensidad(int i) {
+		// TODO Auto-generated method stub
+		this.densidad=i;
+		
+	}
+
+	/*public void setPuntos(List<Punto> puntos2) {
+		// TODO Auto-generated method stub
+		this.puntos=puntos2;
+		
+	}*/
 }
